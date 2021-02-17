25ce37f63f2e1d964d6175c40102a32fc1029d0b
Rishat <samiaulamin@gmail.com>
2021-02-17 14:33:03 +0600

    TASK_C

---
+++ MID_LAB_TASK_PHP_05/TASK_6/TASK_C/blood.php
@@ -0,0 +1,38 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+    if(!empty($_POST['select']))
+     {
+        $selected = $_POST['select'];
+        echo 'You have chosen: '$selected;
+    } 
+    else 
+    {
+        echo 'Please select the value.';
+    }
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="next.php" method="POST">
+	
+<label>Blood Group</label>
+	<select name="select">
+	<option>A+</option>
+	<option>B+</option>
+	<option>AB</option>
+	<option>A-</option>
+	<option>B-</option>
+	<option>AB+</option>
+</select>
+<input type="submit" name="submit" value="Choose option">
+
+</form>
+
+</body>
+</html>
