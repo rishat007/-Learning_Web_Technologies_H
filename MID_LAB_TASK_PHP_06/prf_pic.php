05eda345f3c5334142c3129b22e03d6a2e316007
Rishat <samiaulamin@gmail.com>
2021-02-26 00:47:41 +0600

    Chose Picture

---
+++ MID_LAB_TASK_PHP_06/prf_pic.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form>
+	<fieldset>
+		<legend>PROFILE PICTURE</legend>
+		<table>
+		<tr>
+		
+		<tr>
+			<td>
+		<img src="demo.png" width="100px" height="100px">
+	</td>
+</tr>
+<tr><td>
+		<input type="file" name="file"></td>
+	</tr>
+	<tr>
+		<td>
+		<input type="submit" name="submit"></td>
+	</tr>
+</tr>
+
+</tr>
+</table>
+	</fieldset>
+</form>
+	
+
+</body>
+</html>
