641cde905dd2a3577cffcf4635679a78b6e26666
Rishat <samiaulamin@gmail.com>
2021-03-14 13:21:15 +0600

    user page

---
+++ Mid_lab_Exam/user.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Page</title>
+</head>
+<body>
+	<table  border="1" align="center" width="720px" height="420px">
+		<tr>
+			<td>
+
+
+	<table align="center">
+		<tr>
+			<td >
+				<h3> Welcome Anne! </h3>
+			</td>
+		</tr>
+		<tr>
+			<td align="center" >
+				<a href="profile.php">Profile</a>
+			</td>
+		</tr>
+		<tr>
+			<td  align="center" >
+				<a href="changepass.php">Change Password</a>
+			</td>
+		</tr>
+		
+		<tr>
+			<td  align="center" >
+				<a href="">Logout</a>
+			</td>
+		</tr>
+
+
+</tr>
+</table>
+</td>
+</tr>
+</table>
+
+</body>
+</html>
