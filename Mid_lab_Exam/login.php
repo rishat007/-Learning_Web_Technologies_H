039d0f0f04e0496f8c49c0e24a9ada5e6f738361
Rishat <samiaulamin@gmail.com>
2021-03-14 12:52:06 +0600

    login page

---
+++ Mid_lab_Exam/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form>
+		<fieldset>
+			<legend>LOGIN</legend>
+			<table>
+				<tr>
+					<td>
+						<label>User Id</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="text" name="usename">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Password</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="Password" name="Password">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="submit" name="Login">
+						<a href="reg.php" name="Resister">Resister
+						</a>
+					</td>
+				</tr>
+			</table>
+		</fieldset>
+	</form>
+
+</body>
+</html>
