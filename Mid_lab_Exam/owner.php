dc002ffb71477207e64b6149aaaca7f9b05cbcbc
Rishat <samiaulamin@gmail.com>
2021-03-14 13:17:55 +0600

    welcome nob

---
+++ Mid_lab_Exam/owner.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Page</title>
+</head>
+<body>
+	<table  border="1" align="center" width="720px" height="420px">
+		<tr>
+			<td>
+
+
+	<table align="center">
+		<tr>
+			<td >
+				<b> <b>Welcome Bob!</b></b>
+			</td>
+		</tr>
+		<tr>
+			<td align="center" >
+				<a href="profile.php">Profile</a>
+			</td>
+		</tr>
+		<tr>
+			<td  align="center" >
+				<a href="changepass.php">Change Password</a>
+			</td>
+		</tr>
+		<tr>
+			<td  align="center" >
+				<a href="viwe.php">Viwe User</a>
+			</td>
+		</tr>
+		<tr>
+			<td  align="center" >
+				<a href="">Logout</a>
+			</td>
+		</tr>
+
+
+</tr>
+</table>
+</td>
+</tr>
+</table>
+
+</body>
+</html>
