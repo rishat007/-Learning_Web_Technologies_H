7ddaa9c65b0e72c420e57ea2970d7fe5282e39f9
Rishat <samiaulamin@gmail.com>
2021-03-14 12:34:53 +0600

    registration

---
+++ Mid_lab_Exam/reg.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form>
+		<fieldset>
+			<legend>REGISTRATION</legend>
+
+			<table>
+				<tr>
+					<td>
+						<label>Id</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="number" name="number">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Password</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="Password" name="Password">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Confirm password</label>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="Password" name="Password">
+					</td>
+				</tr>
+				<tr>
+				<td>
+					<label>Name</label>
+				</td>
+				</tr>
+				<tr>
+					<td>
+						<input type="txt" name="name">
+					</td>
+				</tr>
+				<tr>
+					<td>
+					<form>
+  <input type="radio"  name="user" value="user">
+  <label for="male">user</label><br>
+  <input type="radio"  name="Admin" value="Admin">
+  <label for="female">Admin</label><br>
+
+</form> 
+</td>
+</tr>
+<tr>
+	<td>
+		<input type="submit" name="submit"><span>Sing UP</span>
+		<a href="singin.html">Sing In</a>
+	</td>
+</tr>
+			</table>
+		</fieldset>
+	</form>
+
+</body>
+</html>
